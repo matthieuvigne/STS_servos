7d4f5050ec4cb260a1839ff125ed429706950743
botamochi6277 <botamochi6277+git@gmail.com>
2024-10-05 16:43:09 +0900

    Add CI workflow

--- examples/SimpleSweepWithInterfaceBoard/SimpleSweepWithInterfaceBoard.ino
+++ examples/SimpleSweepWithInterfaceBoard/SimpleSweepWithInterfaceBoard.ino
@@ -18,7 +18,8 @@
 // Caution !!!
 // This sketch cannot work with Arduino UNO R3 because it has only single serial
 // port. This sketch needs two ports; one is for serial with PC, the another is
-// for servo control The UNO boars is not suitable for debugging servo control.
+// for servo control. The UNO boards is not suitable for debugging servo
+// control.
 
 #include <Arduino.h>
 
